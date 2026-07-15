2f3d1eebf82a34f27353d276243b91057dd08f2f
ayushsolanki20 <ayushsolanki.045@gmail.com>
2026-07-15 20:58:13 +0530

    .env files and application rooerties configured

--- src/main/java/com/librarymanagement/library_management/controller/LibraryController.java
+++ src/main/java/com/librarymanagement/library_management/controller/LibraryController.java
@@ -25,7 +25,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LibraryController {
 
     private final UserRepository userRepository;
